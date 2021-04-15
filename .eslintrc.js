7c86fff39ff9d9b571a13ec485e36482b3894a59
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2021-04-15 23:05:34 +0200

    Eslint update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,46 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'plugin:react/recommended',
-    'airbnb',
+    'standard'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
     'react',
-    '@typescript-eslint',
+    '@typescript-eslint'
+  ],
+  overrides: [
+    {
+      files: [
+        '**/*.ts',
+        '**/*.tsx'
+      ],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        'no-use-before-define': 'off'
+      }
+    },
+    {
+      files: [
+        '**/*.test.ts',
+        '**/*.test.tsx'
+      ],
+      env: {
+        jest: true
+      }
+    }
   ],
   rules: {
-    indent: ['error', 2],
-  },
-};
+    indent: ['error', 2]
+  }
+}
